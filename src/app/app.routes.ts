288a72239b9f96843b4ec80b678c243b69bd42b6
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-06 13:49:37 +0530

    [Amber] Add. Designed view contact component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
 import { AddContactComponent } from './pages/add-contact/add-contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },  
-  { path: 'add-contact', component: AddContactComponent } 
+  { path: '', component: HomeComponent },
+  { path: 'add-contact', component: AddContactComponent },
+  { path: 'contact-list', component: ContactListComponent },
 ];
